617d1da2d8916ca18cedd9323aecc81fa11af3b1
David Fava <dfava80@gmail.com>
2018-09-28 19:37:10 +0100

    Initial commit

---
+++ themes/rigoblock/public/assets/js/main.js
@@ -0,0 +1,21 @@
+/* ========================================================================
+ * Knowledgebase: main.js v1.0.0
+ * ======================================================================== */
+
+// jQuery(document).ready(function($){
+//   // Defining a function to set size for #hero
+//   function fullscreen(){
+//     jQuery('#hero').css({
+//       width: jQuery(window).width(),
+//       height: jQuery(window).height()
+//     });
+//   }
+//
+//   fullscreen();
+//
+//   // Run the function in case of window resize
+//   jQuery(window).resize(function() {
+//     fullscreen();
+//   });
+//
+// });
